3bfede917a166adc10fbb2e585cc546dd882c892
Hugo Contreras <74910174+Hugo-2@users.noreply.github.com>
2023-10-30 22:53:34 +0100

    Add files via upload

---
+++ GV.cs
@@ -0,0 +1,10 @@
+﻿namespace GoMarket
+{
+    public class GV
+    {
+        /* Ocultar = Hidden */
+        /* Ocultar = visible */
+        public static string ShowMarket_start = "<div class=\"fst-italic\">";
+        public static string ShowMarket_end = "</div>";
+    }
+}
